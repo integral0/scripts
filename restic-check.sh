2b1f674f893e449209e228f6e38fd5b95ce54ae2
Evgeny Sorokin <e.sorokin@southbridge.ru>
2025-06-03 16:12:33 +0300

    update tag to 2.1.0

--- restic-check.sh
+++ restic-check.sh
@@ -2,7 +2,7 @@
 
 PATH=/sbin:/usr/sbin:/usr/local/sbin:/bin:/usr/bin:/usr/local/bin
 
-VERSION=v.1.16.0
+VERSION=v.2.1.0
 #
 # restic-check $VERSION
 #
@@ -26,7 +26,7 @@ function echo_tsn {
 # Nice debugging messages
 function die {
     echo_ts "Error: $1" >&2
-    exit 1;
+    exi_tst 1;
 }
 
 function runSelfUpdate {
@@ -68,6 +68,7 @@ EOF
   exec /bin/bash updateScript.sh
 }
 
+function read_args() {
 if [ -n "$1" ]; then
   while [ -n "$1" ]; do
     case "$1" in
@@ -82,11 +83,14 @@ if [ -n "$1" ]; then
       -h|--help)
         echo "Use: $0 [GLOBAL OPTION]"
         echo "or"
-        echo "Use: $0 [TYPE_BACKUP] [OPTION] [FLAGS]"
+        echo "Use: $0 [TYPE_BACKUP] [CTID] [OPTION]"
         echo ""
         echo "Type backup:"
         echo "-l         | --local               Local backup"
         echo "-r         | --remote              Remote backup"
+        echo ""
+        echo "Ctid:"
+        echo "-id <ctid>               | --ctid <ctid>         Container name"
         echo ""
         echo "Option:"
         echo "-sn                      | --snapshots           All backups snapshots (default)"
@@ -97,9 +101,6 @@ if [ -n "$1" ]; then
         echo "                         | --unlock              Unlock repo"
         echo "                         | --check               Check repo"
         echo "                         | --repair              Repair repo"
-        echo ""
-        echo "Flags:"
-        echo "-id <ctid>               | --ctid <ctid>         Container name"
         echo ""
         echo "Global option:"
         echo "-h                       | --help                Help page"
@@ -114,6 +115,10 @@ if [ -n "$1" ]; then
       -r|--remote)
         #shift
         BACKUP_TYPE="remote"
+      ;;
+      -id|--ctid)
+        shift
+        CUSTOM_CTID="$1"
       ;;
       -sn|--snapshot)
         OPT="snapshots"
@@ -143,19 +148,191 @@ if [ -n "$1" ]; then
       --repair)
         OPT="repair"
       ;;
-      -id|--ctid)
-        shift
-        CUSTOM_CTID="$1"
-      ;;
     esac
     shift
   done
 fi
+}
 
-if [ -f /etc/cron.d/mcbackups ]; then
+function dsbackup_conf {
+  if [ -f $LOCATION/etc/ds-backup.local.conf ]; then
+    die "ERROR !!! ds-backup.local.conf is present. Please remove this file. All parameters must be set in ansible inventory"
+  fi
+  if [ -f $LOCATION/etc/ds-restic-backup.local.conf ]; then
+    die "ERROR !!! ds-restic-backup.local.conf is present. Please remove this file. All parameters must be set in ansible inventory"
+  fi
+  local RESTIC="$(grep -c /srv/southbridge/bin/ds-restic-backup.sh /etc/cron.d/dsbackups)"
+  if (( $RESTIC )); then
+    . "$LOCATION/etc/ds-restic-backup.conf.dist"
+    . "$LOCATION/etc/ds-restic-backup.conf"
+    if [ -z "$REMOTE_BACKUP_HOSTS" ]; then
+      echo_ts "Backup disabled"
+    else
+      echo_ts "Remote backup repo: $REMOTE_BACKUP_HOSTS"
+    fi
+  else
+    . "$LOCATION/etc/ds-backup.conf.dist"
+    . "$LOCATION/etc/ds-backup.conf"
+    if [ -z "$REMOTE_HOSTS" ]; then
+      echo_ts "Backup disabled"
+    else
+      for REMOTE_HOST in $REMOTE_HOSTS; do
+        echo_ts "Remote backup repo: $USERNAME@$REMOTE_HOST::$REMOTE_DIR/$REMOTEHOSTDIR"
+      done
+    fi
+  fi
+
+}
+
+function mcbackup_conf {
+  if [ -f $LOCATION/etc/mc-backup.local.conf ]; then
+    die "ERROR !!! mc-backup.local.conf is present. Please remove this file. All parameters must be set in ansible inventory"
+  fi
+  if [ -f $LOCATION/etc/mc-restic-backup.local.conf ]; then
+    die "ERROR !!! mc-restic-backup.local.conf is present. Please remove this file. All parameters must be set in ansible inventory"
+  fi
+  local RESTIC="$(grep -c /srv/southbridge/bin/mc-restic-backup.sh /etc/cron.d/mcbackups)"
+
+  if (( $RESTIC )); then
+    . "$LOCATION/etc/mc-restic-backup.conf.dist"
+    . "$LOCATION/etc/mc-restic-backup.conf"
+  else
+    . "$LOCATION/etc/mc-backup.conf.dist"
+    . "$LOCATION/etc/mc-backup.conf"
+  fi
+
+  [ ! -d "$CT_PRIVATE" ] && die "\$CT_PRIVATE directory does not exist. ($CT_PRIVATE)"
+  [ "$CTIDS" = "*" ] && die "CTID in \$CT_PRIVATE directory not found."
+
+  echo_ts "Conatiners on host:"
+  for CTID in $CTIDS; do
+    if [ -L "$CT_PRIVATE/$CTID" ]; then
+      PATH_TO_CTID=$(/usr/bin/readlink -f "$CT_PRIVATE/$CTID")
+    else
+      PATH_TO_CTID="$CT_PRIVATE/$CTID"
+    fi
+    if [ -d "$PATH_TO_CTID" ]; then
+      echo -en "$PATH_TO_CTID\t\t"
+      re="\\b$CTID\\b"
+      if [[ ! $CTIDS_EXCLUDE =~ $re ]]; then
+        echo -n " backup"
+        if [[ ! $CTIDS_EXCLUDE_LOCAL =~ $re ]] && [[ -n ${LOCAL_DIR} ]]; then
+          echo -n " local"
+        fi
+        if [[ ! $CTIDS_EXCLUDE_REMOTE =~ $re ]]; then
+          echo -n " remote"
+        fi
+      fi
+      echo
+    fi
+  done
+  echo
+  echo "Local backup repo: ${LOCAL_DIR:-Disabled}"
+  echo "Remote backup repo: $REMOTE_BACKUP_HOSTS"
+}
+
+function openvz_conf {
+  if [ -f $LOCATION/etc/vz-backup.local.conf ]; then
+    die "ERROR !!! vz-backup.local.conf is present. Please remove this file. All parameters must be set in ansible inventory"
+  fi
+  if [ -f $LOCATION/etc/vz-restic-backup.local.conf ]; then
+    die "ERROR !!! vz-restic-backup.local.conf is present. Please remove this file. All parameters must be set in ansible inventory"
+  fi
+  local RESTIC="$(grep -c /srv/southbridge/bin/vz-restic-backup.sh /etc/cron.d/vzbackups)"
+
+  if (( $RESTIC )); then
+    . "$LOCATION/etc/vz-restic-backup.conf.dist"
+    . "$LOCATION/etc/vz-restic-backup.conf"
+    [ ! -d "$CT_PRIVATE" ] && die "\$CT_PRIVATE directory does not exist. ($CT_PRIVATE)"
+    [ "$CTIDS" = "*" ] && die "CTID in \$CT_PRIVATE directory not found."
+
+    echo "Conatiners on host:"
+    for CTID in $CTIDS; do
+      if [ -L "$CT_PRIVATE/$CTID" ]; then
+        PATH_TO_CTID=$(/usr/bin/readlink -f "$CT_PRIVATE/$CTID")
+      else
+        PATH_TO_CTID="$CT_PRIVATE/$CTID"
+      fi
+      if [ -d "$PATH_TO_CTID" ]; then
+        echo -en "$PATH_TO_CTID\t\t"
+        re="\\b$CTID\\b"
+        if [[ ! $CTIDS_EXCLUDE =~ $re ]]; then
+          echo -n " backup"
+          if [[ ! $CTIDS_EXCLUDE_LOCAL =~ $re ]] && [[ -n ${LOCAL_DIR} ]]; then
+            echo -n " local"
+          fi
+          if [[ ! $CTIDS_EXCLUDE_REMOTE =~ $re ]]; then
+            echo -n " remote"
+          fi
+        fi
+        echo
+      fi
+    done
+    echo
+    echo "Local backup repo: ${LOCAL_DIR:-Disabled}"
+    echo "Remote backup repo: $REMOTE_BACKUP_HOSTS"
+  else
+    . "$LOCATION/etc/vz-backup.conf.dist"
+    . "$LOCATION/etc/vz-backup.conf"
+    [ ! -d "$VZ_PRIVATE" ] && die "\$VZ_PRIVATE directory doesn't exist. ($VZ_PRIVATE)"
+    [ "$VEIDS" = "*" ] && die "VEID in \$VZ_PRIVATE directory not found."
+
+    echo "Conatiners on host:"
+    for VEID in $VEIDS; do
+      if [ -L "$VZ_PRIVATE/$VEID" ]; then
+        PATH_TO_VEID=$(/usr/bin/readlink -f "$VZ_PRIVATE/$VEID")
+      else
+        PATH_TO_VEID="$VZ_PRIVATE/$VEID"
+      fi
+      if [ -d "$PATH_TO_VEID" ]; then
+        echo -en "$PATH_TO_VEID\t\t"
+        re="\\b$VEID\\b"
+        if [[ ! $VEIDS_EXCLUDE =~ $re ]]; then
+          echo -n " backup"
+          if [[ ! $VEIDS_EXCLUDE_LOCAL =~ $re ]]; then
+            echo -n " local"
+          fi
+          if [[ ! $VEIDS_EXCLUDE_REMOTE =~ $re ]]; then
+            echo -n " remote"
+          fi
+        fi
+        echo
+      fi
+    done
+    echo
+    echo "Local backup repo: ${LOCAL_DIR:-Disabled}"
+    for REMOTE_HOST in ${REMOTE_HOSTS}; do
+      echo "Remote backup repo: $USERNAME@$REMOTE_HOST::$REMOTE_DIR"
+    done
+  fi
+}
+
+function files_backup_conf {
+  echo_ts "coming soon"
+}
+
+### DS Backup
+function dsbackup {
+  source ${LOCATION}/etc/ds-restic-backup.conf.dist
+  source ${LOCATION}/etc/ds-restic-backup.conf
+  if [ "$BACKUP_TYPE" == "local" ];then
+    echo_ts "Local backup for vds not work... Use -r"
+  elif [ "$BACKUP_TYPE" == "remote" ];then
+    for REMOTE_BACKUP_HOST in ${REMOTE_BACKUP_HOSTS}; do
+      echo_ts "Check REMOTE backup: ${REMOTE_BACKUP_HOST}/${CTID} (${OPT:-snapshots})"
+      LOCAL_AUTH_CONFIG="$LOCATION/etc/ds-restic-url-"$(echo -n "$REMOTE_BACKUP_HOST" | sha256sum | awk '{print $1}')".conf"
+      source $LOCAL_AUTH_CONFIG 2>/dev/null
+      export RESTIC_PASSWORD=${REMOTE_BACKUP_PASSWORD}
+      LLOG=$(set -x; restic -r ${REMOTE_BACKUP_HOST}/${CTID} ${OPT:-snapshots}; set +x)
+      echo "$LLOG"
+    done
+  fi
+}
+
+### MC Backup
+function mcbackup {
   source ${LOCATION}/etc/mc-restic-backup.conf.dist
   source ${LOCATION}/etc/mc-restic-backup.conf
-  echo_ts "Nspawn backup detected."
   if [ -z "$CUSTOM_CTID" ]; then
     CTIDs_LOCAL=$(mctl list | grep -v ^NAME | awk '{print $1}')
     CTIDs_REMOTE="$CTIDs_LOCAL ${HN}_etc"
@@ -195,33 +372,12 @@ if [ -f /etc/cron.d/mcbackups ]; then
       done
     done
   fi
-fi
-
-### Files Backup
-if [ -f /etc/cron.d/filebackups ]; then
-  source ${LOCATION}/etc/file-restic-backup.conf.dist
-  source ${LOCATION}/etc/file-restic-backup.conf
-  echo_ts "Custom files backup detected."
-
-  if [ "$BACKUP_TYPE" == "local" ];then
-    echo_ts "SKIP. Local backup is DISABLED"
-  elif [ "$BACKUP_TYPE" == "remote" ];then
-      for REMOTE_BACKUP_HOST in ${REMOTE_BACKUP_HOSTS}; do
-          echo_ts "CHECK remote files backup: ${REMOTE_BACKUP_PATH}"
-          LOCAL_AUTH_CONFIG="$LOCATION/etc/file-restic-url-"$(echo -n "$REMOTE_BACKUP_HOST" | sha256sum | awk '{print $1}')".conf"
-          source $LOCAL_AUTH_CONFIG 2>/dev/null
-          export RESTIC_PASSWORD=${REMOTE_BACKUP_PASSWORD}
-          LLOG=$(set -x; restic -r ${REMOTE_BACKUP_HOST} ${OPT:-snapshots}; set +x)
-          echo "$LLOG"
-      done
-  fi
-fi
+}
 
 ### VZ Backup
-if [ -f /etc/cron.d/vzbackups ];then
+function openvz {
   source ${LOCATION}/etc/vz-restic-backup.conf.dist
   source ${LOCATION}/etc/vz-restic-backup.conf
-  echo_ts "OpenVZ backup detected."
   if [ -z "$CUSTOM_CTID" ];then CTIDs=$([[ -d /vz/private/ ]]&& cd /vz/private/ && echo *); else CTIDs=$CUSTOM_CTID; fi
   if [ "$BACKUP_TYPE" == "local" -a -n "$LOCAL_DIR" ];then
     for CTID in $CTIDs; do
@@ -248,23 +404,71 @@ if [ -f /etc/cron.d/vzbackups ];then
       done
     done
   fi
+}
+
+### Files Backup
+function filesbackup {
+  source ${LOCATION}/etc/file-restic-backup.conf.dist
+  source ${LOCATION}/etc/file-restic-backup.conf
+  echo_ts "Custom files backup detected."
+
+  if [ "$BACKUP_TYPE" == "local" ];then
+    echo_ts "SKIP. Local backup is DISABLED"
+  elif [ "$BACKUP_TYPE" == "remote" ];then
+      for REMOTE_BACKUP_HOST in ${REMOTE_BACKUP_HOSTS}; do
+          echo_ts "CHECK remote files backup: ${REMOTE_BACKUP_PATH}"
+          LOCAL_AUTH_CONFIG="$LOCATION/etc/file-restic-url-"$(echo -n "$REMOTE_BACKUP_HOST" | sha256sum | awk '{print $1}')".conf"
+          source $LOCAL_AUTH_CONFIG 2>/dev/null
+          export RESTIC_PASSWORD=${REMOTE_BACKUP_PASSWORD}
+          LLOG=$(set -x; restic -r ${REMOTE_BACKUP_HOST} ${OPT:-snapshots}; set +x)
+          echo "$LLOG"
+      done
+  fi
+}
+
+function main {
+if [ -f /etc/cron.d/dsbackups ]; then
+  echo_ts "############################################"
+  echo_ts "VDS backup detected"
+  if [ "$#" -eq 0 ]; then
+    dsbackup_conf
+  else
+    read_args "$@"
+    dsbackup "$@"
+  fi
+elif [ -f /etc/cron.d/mcbackups ]; then
+  echo_ts "############################################"
+  echo_ts "NSPAWN backup detected"
+  if [ "$#" -eq 0 ]; then
+    mcbackup_conf
+  else
+    read_args "$@"
+    mcbackup "$@"
+  fi
+elif [ -f /etc/cron.d/vzbackups ]; then
+  echo_ts "############################################"
+  echo_ts "OpenVZ backup detected"
+  if [ "$#" -eq 0 ]; then
+    openvz_conf
+  else
+    read_args "$@"
+    openvz "$@"
+  fi
+else
+  echo_ts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  echo_ts "Cron task for backup not detected"
 fi
 
-### DS Backup
-if [ -f /etc/cron.d/dsbackups ]; then
-  source ${LOCATION}/etc/ds-restic-backup.conf.dist
-  source ${LOCATION}/etc/ds-restic-backup.conf
-  echo_ts "VDS backup detected"
-  if [ "$BACKUP_TYPE" == "local" ];then
-    echo_ts "Local backup for vds not work... Use -r"
-  elif [ "$BACKUP_TYPE" == "remote" ];then
-    for REMOTE_BACKUP_HOST in ${REMOTE_BACKUP_HOSTS}; do
-      echo_ts "Check REMOTE backup: ${REMOTE_BACKUP_HOST}/${CTID} (${OPT:-snapshots})"
-      LOCAL_AUTH_CONFIG="$LOCATION/etc/ds-restic-url-"$(echo -n "$REMOTE_BACKUP_HOST" | sha256sum | awk '{print $1}')".conf"
-      source $LOCAL_AUTH_CONFIG 2>/dev/null
-      export RESTIC_PASSWORD=${REMOTE_BACKUP_PASSWORD}
-      LLOG=$(set -x; restic -r ${REMOTE_BACKUP_HOST}/${CTID} ${OPT:-snapshots}; set +x)
-      echo "$LLOG"
-    done
+if [ -f /etc/cron.d/filebackups ]; then
+  echo_ts "############################################"
+  echo_ts "Files backup detected"
+  if [ "$#" -eq 0 ]; then
+    filesbackup_conf
+  else
+    read_args "$@"
+    filesbackup "$@"
   fi
 fi
+}
+
+main "$@"
